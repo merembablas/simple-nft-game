2c2ac9c972a220c34bee850db3b1974e2c7263d5
Rifqi Alfian <merembablas@gmail.com>
2021-11-07 22:47:50 +0700

    Init Project

---
+++ src/constants.js
@@ -0,0 +1,13 @@
+const CONTRACT_ADDRESS = '0xc2658e89b665c0CF000340D23cBc6Ee291681925';
+
+const transformCharacterData = (characterData) => {
+  return {
+    name: characterData.name,
+    imageURI: characterData.imageURI,
+    hp: characterData.hp.toNumber(),
+    maxHp: characterData.maxHp.toNumber(),
+    attackDamage: characterData.attackDamage.toNumber(),
+  };
+};
+
+export { CONTRACT_ADDRESS, transformCharacterData };
